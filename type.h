f87b72a8e2334fbc29c48d7f661fbe2a166ef2fe
RubiMaistro <55796855+RubiMaistro@users.noreply.github.com>
2019-12-16 01:20:29 +0100

    types

---
+++ type.h
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int test(string in_text,int &i)
+{
+    i=0;
+        while( in_text[i] )
+        {
+        //Have digit in text?
+        if( isdigit(in_text[i]) )
+        {
+            i++;
+            //isdigit == true;
+            int j=0;
+            bool is_double = false;
+            while( in_text[j] )
+            {
+                if( in_text[j] == '.' ) is_double = true;
+                j++;
+            }
+            //Double
+            if( is_double==true )
+                {
+                    cout<<"\n  You entered text is type of double.\n";
+                break;
+                }
+            //Intiger
+            else if( is_double==false )
+                {
+                    cout<<"\n  You entered text is type of intiger.\n";
+                break;
+                }
+
+        }
+        //Have alpha in text?
+        if( isalpha(in_text[i]) )
+        {
+            i++;
+            //isalpha == true;
+            int j=0;
+            bool is_sentence = false;
+            while( in_text[j] )
+            {
+                //Simple testing
+                //If have a space in_text is a sentence
+                if( in_text[j] == ' ' )
+                {
+                    //Have a space
+                    //Sentence
+                    is_sentence = true;
+                }
+                j++;
+            }
+            //Character
+            if( j==0 )
+            {
+                cout<<"\n  You entered text is a character.\n";
+            break;
+            }
+            //Sentence
+            else if( is_sentence == false )
+            {
+                cout<<"\n  You entered text is word.\n";
+            break;
+            }
+            //A word
+            else if( is_sentence == true )
+            {
+                cout<<"\n  You entered text is a sentence.\n";
+            break;
+            }
+        }
+        i++;
+    }
+    //If not enter ...
+    if(i == 0 )
+    {
+        cout<<"  You didn't enter anything.\n  Try again:\n\n  ";
+        getline(cin,in_text);
+        test(in_text,i);
+    }
+    return i;
+}
+
---
+++ type_identifications.cpp
@@ -0,0 +1,18 @@
+#include "type.h"
+
+using namespace std;
+
+/* Using test function:
+    test ( enter variable , step counter ) */
+
+int main()
+{
+    setlocale(LC_ALL, "" );
+    string in_text;
+    cout<<" Enter something:\n\n  ";
+    getline(cin,in_text);
+    int i=0;
+    test(in_text,i);
+
+return 0;
+}
